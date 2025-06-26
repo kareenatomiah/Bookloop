0f07629fb97a850d4b27a3db5cc3ccbe9cd8627c
kareena tomiah <kareena1508@gmail.com>
2025-06-26 09:27:55 +0400

    preview css

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,3 +4,5 @@ import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 import WebcamController from "./webcam_controller"
 application.register("webcam", WebcamController)
+
+
